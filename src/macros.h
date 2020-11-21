7c9e405b84e7806fb0c4b2920119b89c3d7416ee
Alberto Cenzato <alberto.cenzato@outlook.it>
2020-11-21 09:53:58 +0100

    Macros for cross-platform restrict, constexpr and endianness

---
+++ src/macros.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#if _MSC_VER
+#define _restrict __restrict
+#elif ARDUINO_AVR_UNO
+#define _restrict
+#else
+#define _restrict __restrict__
+#endif
+
+#if ARDUINO_AVR_UNO
+#define _constexpr 
+#else
+#define _constexpr constexpr
+#endif
+
+#if _MSC_VER
+#define LITTLE_ENDIAN true
+#elif ARDUINO_AVR_UNO
+#define LITTLE_ENDIAN true
+#else 
+#define LITTLE_ENDIAN true
+#endif
