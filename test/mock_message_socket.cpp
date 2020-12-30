b4407ba85ab01691b498492a531136fb430b9f89
Alberto Cenzato <alberto.cenzato@outlook.it>
2020-12-30 12:36:10 +0100

    MockMessageSocket can use preallocated buffers

--- test/mock_message_socket.cpp
+++ test/mock_message_socket.cpp
@@ -3,7 +3,26 @@
 
 namespace com {
 
-MockMessageSocket::MockMessageSocket() : is_connected(false){}
+MockMessageSocket::MockMessageSocket(
+  std::array<std::uint8_t, 1024>* send_buffer,
+  std::array<std::uint8_t, 1024>* receive_buffer) 
+  : is_connected(false), 
+    send_buffer(send_buffer), 
+    receive_buffer(receive_buffer), 
+    alloc_send(send_buffer==nullptr), 
+    alloc_receive(receive_buffer==nullptr) {
+  if (alloc_send)
+    send_buffer = new std::array<std::uint8_t, 1024>();
+  if (alloc_receive)
+    receive_buffer = new std::array<std::uint8_t, 1024>();
+}
+
+MockMessageSocket::~MockMessageSocket() {
+  if (alloc_send)
+    delete send_buffer;
+  if (alloc_receive)
+    delete receive_buffer;
+}
 
 bool MockMessageSocket::connect() {
   if (is_connected)
@@ -17,17 +36,17 @@ void MockMessageSocket::disconnect() {
 }
 
 bool MockMessageSocket::send(const void* data, std::size_t bytes) {
-  if (bytes > send_receive_buffer.size())
+  if (bytes > send_buffer->size())
     return false;
 
-  utils::copyToLittleEndian(bytes, send_receive_buffer.data());
-  memcpy(send_receive_buffer.data() + sizeof(bytes), data, bytes);
+  utils::copyToLittleEndian(bytes, send_buffer->data());
+  memcpy(send_buffer->data() + sizeof(bytes), data, bytes);
   return true;
 }
 
 std::size_t MockMessageSocket::receive(void* data) {
-  std::size_t length = utils::copyFromLittleEndian<std::size_t>(send_receive_buffer.data());
-  memcpy(data, send_receive_buffer.data() + sizeof(length), length);
+  std::size_t length = utils::copyFromLittleEndian<std::size_t>(receive_buffer->data());
+  memcpy(data, receive_buffer->data() + sizeof(length), length);
   return length;
 }
 
--- test/mock_message_socket.h
+++ test/mock_message_socket.h
@@ -8,15 +8,21 @@ namespace com {
 class MockMessageSocket : public IMessageSocket 
 {
 public:
-  MockMessageSocket();
+  MockMessageSocket(
+    std::array<std::uint8_t, 1024>* send_buffer = nullptr, 
+    std::array<std::uint8_t, 1024>* receive_buffer = nullptr);
+  ~MockMessageSocket();
   bool connect() override;
   void disconnect() override;
   bool send(const void* data, size_t bytes) override;
   size_t receive(void* data) override;
 
 private:
+  const bool alloc_send;
+  const bool alloc_receive;
   bool is_connected;
-  std::array<std::uint8_t, 1024> send_receive_buffer;
+  std::array<std::uint8_t, 1024>* send_buffer;
+  std::array<std::uint8_t, 1024>* receive_buffer;
 };
 
 }
