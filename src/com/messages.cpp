8ecdb1cbb7426567897dfeac980e540831c8f042
Alberto Cenzato <alberto.cenzato@outlook.it>
2021-01-04 17:40:21 +0100

    use string.h and stdlib.h instead of c++ version

--- src/com/messages.cpp
+++ src/com/messages.cpp
@@ -1,20 +1,20 @@
 #include "messages.h"
 
-#include <cstring>
-#include <cstdlib>
+#include <string.h>
+#include <stdlib.h>
 
 namespace com {
 
 void BinaryMessage::mallocAndSet(uint32_t payload_length, const uint8_t* payload) {
   this->payload_length = payload_length;
   freeMemory();
-  this->payload = reinterpret_cast<uint8_t*>(std::malloc(payload_length));
-  std::memcpy(this->payload, payload, payload_length);
+  this->payload = reinterpret_cast<uint8_t*>(malloc(payload_length));
+  memcpy(this->payload, payload, payload_length);
 }
 
 void BinaryMessage::freeMemory() {
   if (payload)
-    std::free(payload);
+    free(payload);
   payload = nullptr;
 }
 
