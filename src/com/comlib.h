03a1dfa99c4b3d162f3dc04e06133c09e9321207
Alberto Cenzato <alberto.cenzato@outlook.it>
2021-01-04 16:53:11 +0100

    add comlib.h as unique entry point for arduino

---
+++ src/com/comlib.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "macros.h"
+#include "communication.h"
+#include "communication_interface.h"
+#include "messages.h"
+#include "serialization.h"
+#include "stream_message_reader.h"
+#include "utils.h"
