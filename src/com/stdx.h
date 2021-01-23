b89492d054ddb236aba653c548b00e7bbf70860f
Alberto Cenzato <alberto.cenzato@outlook.it>
2021-01-23 17:28:38 +0100

    Improved UPtr

--- src/com/stdx.h
+++ src/com/stdx.h
@@ -12,6 +12,11 @@ void swap(T& a, T& b) {
   b = tmp;
 }
 
+template <class T>
+T&& move(T& a) {
+  return static_cast<T&&>(a);
+}
+
 
 template <class T>
 class UPtr;
@@ -25,6 +30,7 @@ class UPtr
 public:
   UPtr() : owned_ptr(nullptr) {}
   UPtr(T*&& ptr) : owned_ptr(ptr) {}
+  UPtr(nullptr_t ptr) : UPtr() {}
 
   UPtr(const UPtr<T>& ptr) = delete;
   UPtr(UPtr<T>&& ptr) : UPtr<T>() {
@@ -42,6 +48,8 @@ public:
       delete owned_ptr;
   }
 
+  operator bool() const { return owned_ptr; }
+
   friend void swap<T>(UPtr<T>& ptr1, UPtr<T>& ptr2);
 
   T& operator*() { return *this->owned_ptr; }
@@ -49,9 +57,28 @@ public:
   T* operator->() { return this->owned_ptr; }
   const T* operator->() const { return this->owned_ptr; }
 
+  const T* get() const { return owned_ptr; }
+  T* get() { return owned_ptr; }
+
 private:
   T* owned_ptr;
 };
+
+template <class T>
+bool operator==(const UPtr<T>& a, const UPtr<T>& b) { return a.get() == b.get(); }
+template <class T>
+bool operator!=(const UPtr<T>& a, const UPtr<T>& b) { return !(a == b); }
+
+template <class T>
+bool operator==(nullptr_t a, const UPtr<T>& b) { return UPtr<T>(a) == b; }
+template <class T>
+bool operator!=(nullptr_t a, const UPtr<T>& b) { return !(a == b); }
+
+template <class T>
+bool operator==(const UPtr<T>& a, nullptr_t b) { return a == UPtr<T>(b); }
+template <class T>
+bool operator!=(const UPtr<T>& a, nullptr_t b) { return !(a == b); }
+
 
 template <class T>
 void swap(UPtr<T>& ptr1, UPtr<T>& ptr2) {
