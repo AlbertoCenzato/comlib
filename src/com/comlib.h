8581b260890129aba76aa43c894b9c733f0fcca2
Alberto Cenzato <alberto.cenzato@covisionlab.com>
2021-08-14 17:09:36 +0200

    Do not include nonexistent messages.h

--- src/com/comlib.h
+++ src/com/comlib.h
@@ -3,7 +3,6 @@
 #include "macros.h"
 #include "communication.h"
 #include "communication_interface.h"
-#include "messages.h"
 #include "serialization.h"
 #include "stream_message_reader.h"
 #include "utils.h"
