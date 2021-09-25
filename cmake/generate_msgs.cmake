d5c224009fef2af9e33049ec7c829b1c9dcbeb25
Alberto Cenzato <alberto.cenzato@outlook.it>
2021-09-25 10:53:46 +0200

    Add possibility for external projects to add messages

--- cmake/generate_msgs.cmake
+++ cmake/generate_msgs.cmake
@@ -1,29 +1,37 @@
-message ("---------------------------")
-message ("| Message code generation |")
-message ("---------------------------")
+function (add_message msg_file)
+	set(MSGS_FILES "${MSGS_FILES};${msg_file}" CACHE INTERNAL "")
+endfunction()
 
-file (GLOB MSGS_GEN ${CMAKE_CURRENT_SOURCE_DIR}/msg/*.msg)
-
-set(TMP_MSG_SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/msg/src)
-file(MAKE_DIRECTORY ${TMP_MSG_SRC_DIR})
-foreach(file ${MSGS_GEN})
-	message("Generating C++ code for ${file}")
-	execute_process(COMMAND racket ${CMAKE_CURRENT_SOURCE_DIR}/msg_generation/message_generator.rkt ${file} ${TMP_MSG_SRC_DIR}
-					RESULT_VARIABLE result)
-	message(${result}) 
-endforeach()
-
-file (GLOB MSGS_SRC ${TMP_MSG_SRC_DIR}/*.cpp)
-file (GLOB MSGS_HEADERS ${TMP_MSG_SRC_DIR}/*.h)
-
-file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/src/msg)
-foreach(file ${MSGS_SRC})
-	configure_file(${file} ${CMAKE_CURRENT_SOURCE_DIR}/src/msg)
-endforeach()
-
-file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/include/com/msg)
-foreach(file ${MSGS_HEADERS})
-	configure_file(${file} ${CMAKE_CURRENT_SOURCE_DIR}/include/com/msg)
-endforeach()
-
-file(REMOVE_RECURSE ${TMP_MSG_SRC_DIR})
+function (generate_message_code)
+	message ("---------------------------")
+	message ("| Message code generation |")
+	message ("---------------------------")
+	
+	file (GLOB DEFAULT_MSGS_FILES ${CMAKE_CURRENT_SOURCE_DIR}/msg/*.msg)
+	set (MSGS_FILES "${DEFAULT_MSGS_FILES};${MSGS_FILES}" CACHE INTERNAL "")
+	message ("Generating messages from: ${MSGS_FILES}")
+	
+	set(TMP_MSG_SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/msg/src)
+	file(MAKE_DIRECTORY ${TMP_MSG_SRC_DIR})
+	foreach(file ${MSGS_FILES})
+		message("Generating C++ code for ${file}")
+		execute_process(COMMAND racket ${CMAKE_CURRENT_SOURCE_DIR}/msg_generation/message_generator.rkt ${file} ${TMP_MSG_SRC_DIR}
+						RESULT_VARIABLE result)
+		message(${result}) 
+	endforeach()
+	
+	file (GLOB MSGS_SRC ${TMP_MSG_SRC_DIR}/*.cpp)
+	file (GLOB MSGS_HEADERS ${TMP_MSG_SRC_DIR}/*.h)
+	
+	file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/src/msg)
+	foreach(file ${MSGS_SRC})
+		configure_file(${file} ${CMAKE_CURRENT_SOURCE_DIR}/src/msg)
+	endforeach()
+	
+	file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/include/com/msg)
+	foreach(file ${MSGS_HEADERS})
+		configure_file(${file} ${CMAKE_CURRENT_SOURCE_DIR}/include/com/msg)
+	endforeach()
+	
+	file(REMOVE_RECURSE ${TMP_MSG_SRC_DIR})
+endfunction()
